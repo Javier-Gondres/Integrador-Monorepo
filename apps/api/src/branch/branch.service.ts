7780f21d4f3a10ce83c0122e2f8008b03cc229ac
Jose A. Hidalgo Garcia <JAHG0002@ce.pucmm.edu.do>
2026-05-26 00:11:38 -0400

    implementacion servicios de branch

---
+++ apps/api/src/branch/branch.service.ts
@@ -0,0 +1,78 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { prisma } from "@repo/db";
+
+import { CreateBranchDto } from "./dto/create-branch.dto";
+import { UpdateBranchDto } from "./dto/update-branch.dto";
+
+@Injectable()
+export class BranchService {
+  async create(createBranchDto: CreateBranchDto, companySlug: string) {
+    const company = await prisma.company.findUnique({
+      where: {
+        slug: companySlug,
+      },
+    });
+
+    if (!company) {
+      throw new NotFoundException(`Empresa slug: ${companySlug} no encontrada`);
+    }
+
+    return await prisma.branch.create({
+      data: {
+        name: createBranchDto.name,
+        address: createBranchDto.address,
+        phone: createBranchDto.phone,
+        companyId: company.id,
+      },
+    });
+  }
+
+  async findAll(slug: string) {
+    return await prisma.branch.findMany({
+      where: {
+        slug,
+        isActive: true,
+      },
+    });
+  }
+
+  async findOne(id: string) {
+    const branch = await prisma.branch.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!branch) {
+      throw new NotFoundException(`Sucursal Id: ${id} no encontrado`);
+    }
+
+    return branch;
+  }
+
+  async update(id: string, updateBranchDto: UpdateBranchDto) {
+    const branch = await this.findOne(id);
+
+    return await prisma.branch.update({
+      where: {
+        id: branch.id,
+      },
+      data: {
+        name: updateBranchDto.name,
+        address: updateBranchDto.address,
+        phone: updateBranchDto.phone,
+      },
+    });
+  }
+
+  async remove(id: string) {
+    const branch = await this.findOne(id);
+
+    return await prisma.branch.update({
+      where: { id: branch.id },
+      data: {
+        isActive: false,
+      },
+    });
+  }
+}
---
+++ apps/api/src/branch/dto/create-branch.dto.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty, IsOptional } from "class-validator";
+
+export class CreateBranchDto {
+  constructor(name: string, address?: string, phone?: string) {
+    this.name = name;
+    this.address = address;
+    this.phone = phone;
+  }
+
+  @IsNotEmpty({ message: "el nombre no puede estar vacío" })
+  name: string;
+
+  @IsOptional()
+  address?: string;
+
+  @IsOptional()
+  phone?: string;
+}
