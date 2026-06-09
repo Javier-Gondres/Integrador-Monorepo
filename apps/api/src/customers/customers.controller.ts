1a5552e64384a2304ab3f4b15a9015b6fc346e12
falmonted <fer.almonte.delgado@gmail.com>
2026-06-09 10:57:14 -0400

    Agregando Controladores de Clientes

---
+++ apps/api/src/customers/customers.controller.ts
@@ -0,0 +1,67 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CompanyId, RequireCompany } from 'src/common/company';
+
+import { CustomersService } from './customers.service';
+import { CreateCustomerDto } from './dto/create-customer.dto';
+import { QueryCustomersDto } from './dto/query-customers.dto';
+import { UpdateCustomerDto } from './dto/update-customer.dto';
+
+@Controller('customers')
+export class CustomersController {
+  constructor(private readonly customersService: CustomersService) {}
+
+  @RequireCompany()
+  @Get()
+  findAll(@CompanyId() companyId: string, @Query() query: QueryCustomersDto) {
+    return this.customersService.findPaginatedByCompany(companyId, query);
+  }
+
+  @RequireCompany()
+  @Get(':id')
+  findById(@Param('id') id: string, @CompanyId() companyId: string) {
+    return this.customersService.findByIdInCompany(id, companyId);
+  }
+
+  @RequireCompany()
+  @Post()
+  create(@Body() dto: CreateCustomerDto, @CompanyId() companyId: string) {
+    return this.customersService.create(companyId, dto);
+  }
+
+  @RequireCompany()
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() dto: UpdateCustomerDto,
+    @CompanyId() companyId: string,
+  ) {
+    return this.customersService.update(id, companyId, dto);
+  }
+
+  @RequireCompany()
+  @Patch(':id/activate')
+  activate(@Param('id') id: string, @CompanyId() companyId: string) {
+    return this.customersService.activate(id, companyId);
+  }
+
+  @RequireCompany()
+  @Patch(':id/deactivate')
+  deactivate(@Param('id') id: string, @CompanyId() companyId: string) {
+    return this.customersService.deactivate(id, companyId);
+  }
+
+  @RequireCompany()
+  @Delete(':id')
+  remove(@Param('id') id: string, @CompanyId() companyId: string) {
+    return this.customersService.remove(id, companyId);
+  }
+}
