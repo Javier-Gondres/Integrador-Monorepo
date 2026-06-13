f7669ee240dbe6e3a55f023c8ba9af5ead895a1b
Joseph Muñoz <JRMP0002@ce.pucmm.edu.do>
2026-06-13 11:56:47 -0300

    feat: add cash register query DTO, integrate CashRegistersModule, and implement sidebar UI component

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -12,7 +12,7 @@ import { MeModule } from './me/me.module';
 import { ProductsModule } from './products/products.module';
 import { SuppliersModule } from './suppliers/suppliers.module';
 import { UsersModule } from './users/users.module';
-
+import { CashRegistersModule } from './cash-registers/cash-registers.module';
 const envFilePath =
   process.env.APP_ENV === 'staging' ? '.env.staging' : '.env.development';
 
@@ -31,6 +31,7 @@ const envFilePath =
     MeModule,
     EmployeesModule,
     SuppliersModule,
+    CashRegistersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
