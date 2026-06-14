5387c19952a114605696e1f36a35c679ed4fdcde
Jose A. Hidalgo Garcia <JAHG0002@ce.pucmm.edu.do>
2026-06-13 21:55:47 -0400

    extension de los tipos productos/proveedor

--- apps/web/src/modules/products/api/get-products.ts
+++ apps/web/src/modules/products/api/get-products.ts
@@ -10,6 +10,7 @@ export async function getProducts(filters?: ProductFilters) {
       take: filters?.take,
       search: filters?.search,
       categoryId: filters?.categoryId,
+      excludeSupplierId: filters?.excludeSupplierId,
       isActive: filters?.isActive,
     },
   });
