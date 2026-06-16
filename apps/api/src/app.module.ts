4752d79c3f4cf7c6627ed667f241093783467107
Jose A. Hidalgo Garcia <JAHG0002@ce.pucmm.edu.do>
2026-06-16 10:18:21 -0400

    arreglo a imports en app.module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,18 +5,20 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
 import { BranchModule } from "./branch/branch.module";
-import { CashRegistersModule } from './cash-registers/cash-registers.module';
+import { CashRegistersModule } from "./cash-registers/cash-registers.module";
 import { CategoriesModule } from "./categories/categories.module";
 import { CompanyModule } from "./company/company.module";
-import { DiscountsModule } from './discounts/discounts.module';
+import { CustomersModule } from "./customers/customers.module";
+import { DiscountsModule } from "./discounts/discounts.module";
 import { EmployeesModule } from "./employees/employees.module";
 import { InventoryModule } from "./inventory/inventory.module";
-import { InventoryMovementModule } from './inventory-movement/inventory-movement.module';
+import { InventoryMovementModule } from "./inventory-movement/inventory-movement.module";
 import { MeModule } from "./me/me.module";
-import { ProductSupplierModule } from './product-supplier/product-supplier.module';
+import { ProductSupplierModule } from "./product-supplier/product-supplier.module";
 import { ProductsModule } from "./products/products.module";
 import { PurchasesModule } from "./purchases/purchases.module";
 import { SuppliersModule } from "./suppliers/suppliers.module";
+import { UsersModule } from "./users/users.module";
 
 const envFilePath =
   process.env.APP_ENV === "staging" ? ".env.staging" : ".env.development";
