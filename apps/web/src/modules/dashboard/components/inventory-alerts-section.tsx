1d58a0cec40a2b32267085faf0d8f6ffea626f80
falmonted <fer.almonte.delgado@gmail.com>
2026-07-07 14:21:19 -0400

    cambio de icono tarjeta

--- apps/web/src/modules/dashboard/components/inventory-alerts-section.tsx
+++ apps/web/src/modules/dashboard/components/inventory-alerts-section.tsx
@@ -212,7 +212,7 @@ export function InventoryAlertsSection() {
                 color: "#fff",
               }}
             >
-              <PackageSearch size={18} />
+              <AlertTriangle size={18} />
             </div>
             <div>
               <div
