f1cba5d7d9caab9a23ee7e4c4fe3c1d372178cb5
Javier-Gondres <jegd0001@ce.pucmm.edu.do>
2026-08-11 16:02:48 -0400

    Eliminando la pantalla de empresa

--- apps/web/src/modules/companies/types/company.types.ts
+++
@@ -1,9 +0,0 @@
-export type CompanyListItem = {
-  id: string;
-  name: string;
-  slug: string;
-  isActive: boolean;
-  role: string;
-  defaultBranchId: string | null;
-  rnc?: string | null;
-};
